6c2fc8adf758c8470182dda540e2ec387bc08590
Edidiong Archibong <eddymyk13@gmail.com>
2026-01-18 18:33:40 +0100

    Make some changes

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -13,14 +13,16 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
     books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
         return self.name
-    
+
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
