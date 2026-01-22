60d8b94edabaec5820628eff12cc2b7e030406c3
Edidiong Archibong <eddymyk13@gmail.com>
2026-01-22 20:18:22 +0100

    Update list_books view to use Book.objects.all()

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
 
 # Function-based view: list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()  
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
