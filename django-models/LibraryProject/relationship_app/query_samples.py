b4f495714ed2344d4e42a0a9cb221a8bd192e7f9
Edidiong Archibong <eddymyk13@gmail.com>
2026-01-22 14:20:43 +0100

    Fix queries in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,20 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+# --- Queries ---
 
-# Query all books by a specific author
-def get_books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
-
-
-# List all books in a library
-def get_books_in_library(library_name):
-    return Book.objects.filter(libraries__name=library_name)
+# 1️⃣ Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return author.books.all()
 
 
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    return Librarian.objects.get(library__name=library_name)
+# 2️⃣ List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+# 3️⃣ Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
