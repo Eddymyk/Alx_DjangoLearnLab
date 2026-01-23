0f63a237bc080ba0ed20e63cc0e5768d536e1eef
Edidiong Archibong <eddymyk13@gmail.com>
2026-01-23 21:14:08 +0100

    Update relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,16 @@
+# relationship_app/urls.py
 from django.urls import path
 from django.views.generic import RedirectView
-from . import views  # import all views
 from django.contrib.auth.views import LoginView, LogoutView
-
-
+from . import views  # import all views
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/books/"), name="home"),
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("books/", views.list_books, name="list_books"),  # use views.list_books
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    # Use Django built-in LoginView and LogoutView directly
+    # Authentication
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    
-    # Register view
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),  # use views.register
 ]
