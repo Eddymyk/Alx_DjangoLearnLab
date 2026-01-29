06bbdaa173eef931bf27d4f54b8ed28af5f6de17
Edidiong Archibong <eddymyk13@gmail.com>
2026-01-29 17:45:14 +0100

    Add BookViewSet with full CRUD and router configuration

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# Existing list view
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for full CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing Book instances.
+    Handles: list, create, retrieve, update, destroy
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
