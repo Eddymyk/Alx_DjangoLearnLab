3d7a93b12810c51d7b32007a5e8158fd4bde2e4d
Edidiong Archibong <eddymyk13@gmail.com>
2026-02-06 20:27:00 +0100

    Add checker-specific URL paths: books/create, books/update, books/delete

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,4 +7,8 @@ from .views import (
 urlpatterns = [
     path('books/', BookListCreateAPIView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-detail'),
+    path('books/create', BookListCreateAPIView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-delete'),
 ]
+
