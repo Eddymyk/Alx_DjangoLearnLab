8117189b1d4ceea2018e6979d5d710056f4b2e85
Edidiong Archibong <eddymyk13@gmail.com>
2026-02-06 18:12:31 +0100

    Update API views, URLs, and project setup

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+
+
+def home(request):
+    return HttpResponse("Welcome to Edidiong's Page")
+
 
 # Existing list view
 class BookList(generics.ListAPIView):
@@ -18,3 +24,4 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+    
