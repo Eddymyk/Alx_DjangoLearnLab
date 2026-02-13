1541e8477cde335802494cee871611041ef8cedb
Edidiong Archibong <eddymyk13@gmail.com>
2026-02-13 20:26:55 +0100

    Update urls.py file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -26,9 +26,10 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-add'),
+    path('post/comments/new/', CommentCreateView.as_view(), name='comment-new'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+
 
 
 
