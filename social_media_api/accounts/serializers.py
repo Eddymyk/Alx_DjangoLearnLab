848d157636a84e7675940b0255788815526b36b2
Edidiong Archibong <eddymyk13@gmail.com>
2026-02-19 19:33:36 +0100

    Fix serializers to use get_user_model and token creation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -20,19 +20,18 @@ class RegisterSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        # Create user
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
         )
 
-        # Extra fields
+        # Extra profile fields
         user.bio = validated_data.get('bio', '')
         user.profile_picture = validated_data.get('profile_picture')
         user.save()
 
-        # Create token
+        # Create authentication token
         Token.objects.create(user=user)
 
         return user
